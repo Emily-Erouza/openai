c36ea3fbf684efeb3906421adceb88a2a426de9f
Emily-Erouza <emilysbongile17@gmail.com>
2024-02-15 15:28:09 -0500

    add openai

---
+++ script.js
@@ -0,0 +1,26 @@
+// import { config } from "dotenv"
+
+
+// config();
+
+// import {Configuration, OpenApi} from "openai"
+// const openai = new OpenApi(new Configuration({
+// apiKey: process.env.API_KEY
+
+// }))
+
+// userInterface.prompt()
+// userInterface.on("line",async input =>{
+//     const res =await openai
+//     .createChatCompletion({
+//         model:"gpt-3.5-turbo",
+//         message:[{role:"user", content:"hello chat gpt"}]
+// })
+// })
+// // }).then(res =>{
+
+// //     console.log(res.data.choices[0].message.content)
+// // })
+
+
+
